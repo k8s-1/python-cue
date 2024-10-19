d7e295471deedf4dbed12f74e7e72461028f4733
pman <pman-$-432894328923@gmail.com>
2024-10-19 13:26:58 +0200

    chore: update file(s):

---
+++ main.py
@@ -0,0 +1,24 @@
+import yaml
+
+with open('values.yaml', 'r') as file:
+    prime_service = yaml.safe_load(file)
+
+content = """import (
+    "github.com/k8s-1/app/best"
+)
+
+#objects: best.#Def & {
+    #vals: {
+        """
+
+content += f"""
+        someval: "{prime_service['rest']['url']}"
+    """
+
+content += """
+    }
+}
+yaml.MarshalStream([ for _, o in #objects {o} ])
+"""
+
+print(content)
